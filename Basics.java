93218c49b17325023997791548efa0be48a405f4
Sweta <kumarisweta.as@gmail.com>
2022-05-08 22:01:47 +0530

    Add files via upload

---
+++ Basics.java
@@ -0,0 +1,23 @@
+public class Basics {
+    public static void main(String[] args) {
+        //if condition
+//        int a=10;
+//        if (a == 10){
+//            System.out.println("Hello World");
+//        }
+
+        //while loop
+//        int count = 1;
+//        while(count != 5){
+//            System.out.println(count);
+//            count++;
+//        }
+
+        //for loop
+        for(int count = 1; count!=5; count++){
+            System.out.println(count);
+        }
+        //when you DON'T know how many times the loop is going to run, use the WHILE loop
+        //when you know how many times the loop is going to run, use the FOR loop
+    }
+}
