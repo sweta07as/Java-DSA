497656ee5381b326377d39ffe2433507e5c65a21
Sweta <kumarisweta.as@gmail.com>
2022-06-23 19:25:16 +0530

    Update Grades.java

--- methods/assignment/Grades.java
+++ methods/assignment/Grades.java
@@ -1,3 +1,4 @@
+//Q8 Write a program that will ask the user to enter his/her marks (out of 100). Define a method that will display grades according to the marks entered as below:
 import java.util.Scanner;
 
 public class Grades {
