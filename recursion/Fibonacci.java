5313feaded482c1496970fc43203be20daf0be90
Sweta <kumarisweta.as@gmail.com>
2022-08-19 09:43:49 +0530

    Update Fibonacci.java

--- recursion/Fibonacci.java
+++ recursion/Fibonacci.java
@@ -5,6 +5,12 @@ public class Fibonacci {
     public static void main(String[] args) {
         int ans = fibo(5);
         System.out.println(ans);
+        
+        //from Time and Space Complexity lecture:-
+        for (int i = 0; i < 11; i++) {
+            System.out.println(fiboFormula(i));
+        }
+        System.out.println(fiboFormula(50));
     }
     static int fibo(int n){
 
@@ -15,6 +21,11 @@ public class Fibonacci {
 //        When you write the recursion (i.e., recursive solution) in a formula, it is called recurrence relation.
 //        Here the recurrence relation is, fibo(n) = fibo(n-1) + fibo(n-2)
         return fibo(n-1) + fibo(n-2);
+    }
+    
+    //from Time and Space Complexity lecture :-
+    static int fiboFormula(int n){
+        return (int)((Math.pow(((1 + Math.sqrt(5))/2), n) - Math.pow(((1 - Math.sqrt(5))/2), n)) / Math.sqrt(5));
     }
 }
 //Building recursive tree and using debug pointer are two ways to know the flow of program.
