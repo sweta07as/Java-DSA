8248197f9882428b865ef8a5b576d4273539ab1c
Sweta <kumarisweta.as@gmail.com>
2022-12-01 23:59:33 +0530

    Add files via upload

---
+++ 14 oops/b/Greeting.java
@@ -0,0 +1,7 @@
+package oops2.b;
+//In simple terms, a package is just a folder
+public class Greeting {
+    public static void main(String[] args) {
+        System.out.println("Well done");
+    }
+}
