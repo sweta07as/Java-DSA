c0c07d5cfb74fa667ea5a0bd41c108f80b4936fa
Sweta <kumarisweta.as@gmail.com>
2022-12-01 23:59:00 +0530

    Add files via upload

---
+++ 14 oops/Message.java
@@ -0,0 +1,11 @@
+package oops2.b;
+
+public class Message {
+    public static void main(String[] args) {
+
+    }
+
+    public static void message() {
+        System.out.println("This is the message");
+    }
+}
